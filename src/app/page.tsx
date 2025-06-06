fa5c27c8e89d7909d2b0ee06336eb2ee3e5b8420
AdolfChan <sarebugen@gmail.com>
2025-06-06 19:46:15 +0500

    fixed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,11 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 flex flex-col gap-[32px] items-center sm:items-start p-4 sm:p-20">
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#6489da] text-white gap-2 hover:bg-[#6489da] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </Link>
-          <Link
-            className="rounded-full border border-solid border-[#6489da] transition-colors flex items-center justify-center hover:bg-[#6489da] hover:text-white text-[#6489da] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </Link>
-        </div> */}
-      </main>
+      <main className="flex-1 flex flex-col gap-[32px] items-center sm:items-start p-4 sm:p-20"></main>
       <Footer />
     </div>
   );
