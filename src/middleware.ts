5dbe395abfaede7d816de0b41b76919f17d8a82b
AdolfChan <sarebugen@gmail.com>
2025-06-29 13:43:31 +0500

    zaebalsya2

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,25 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-  console.log("TOKEN MIDDLEWARE", token);
-  const { pathname } = req.nextUrl;
-  const protectedPaths = ["/profile"];
-  console.log("HEADERS", Object.fromEntries(req.headers.entries()));
-  const isProtectedPath = protectedPaths.some((path) =>
-    pathname.startsWith(path)
-  );
+  try {
+    const token = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+    console.log("TOKEN MIDDLEWARE", token);
+    const { pathname } = req.nextUrl;
+    const protectedPaths = ["/profile"];
+    console.log("HEADERS", Object.fromEntries(req.headers.entries()));
+    const isProtectedPath = protectedPaths.some((path) =>
+      pathname.startsWith(path)
+    );
 
-  if (isProtectedPath && !token) {
-    const loginUrl = new URL("/login", req.url);
-    return NextResponse.redirect(loginUrl);
+    if (isProtectedPath && !token) {
+      const loginUrl = new URL("/login", req.url);
+      return NextResponse.redirect(loginUrl);
+    }
+  } catch (err) {
+    console.error("Ошибка getToken в middleware:", err);
   }
 
   return NextResponse.next();
